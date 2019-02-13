72485c8223dfd882cf8e368030ef716a6b329ac1
zkatemor <rinaholmes7@gmail.com>
2019-02-13 23:54:28 +0300

    second hometask

---
+++ two_activity/app/src/main/java/com/zkatemor/two_activity/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.zkatemor.two_activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+    }
+}
+
